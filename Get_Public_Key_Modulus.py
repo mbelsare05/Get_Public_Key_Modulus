e1640a6ad3c2b71bc98def5fdd7f0ed5d87aed7b
root <mbelsare05@gmail.com>
2017-05-06 21:40:21 -0700

    source file

---
+++ Get_Public_Key_Modulus.py
@@ -0,0 +1,20 @@
+import Crypto.PublicKey.RSA
+import M2Crypto
+import sys
+
+x509 = M2Crypto.X509.load_cert(sys.argv[1],0)
+publickey = x509.get_pubkey()
+key = Crypto.PublicKey.RSA.importKey(publickey.as_der())
+
+modulus = key.n
+public_exponent = key.e
+
+f =  open(sys.argv[2],'wb')
+f.write(format(modulus,'02X'))
+f.close()
+
+print format(modulus,'02X')
+print 
+
+print 
+print hex(public_exponent)
